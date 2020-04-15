7ff3175b2d7f5af9d70672e300e92ed7336f86a8
meliao <owenjmelia@gmail.com>
2020-04-15 13:17:03 -0400

    Changes to allow liftover in covariance_for_model

--- src/covariance_for_model.py
+++ src/covariance_for_model.py
@@ -9,6 +9,7 @@ import numpy
 import gzip
 
 from pyarrow import parquet as pq
+import pyliftover
 
 from genomic_tools_lib import Logging, Utilities
 from genomic_tools_lib.data_management import TextFileTools
@@ -25,12 +26,108 @@ def get_file_map(args):
         p[k] = g
     return p
 
+def _lift(l, chromosome, position):
+    # NA is important, instead of None or NaN, so that integer positions are not converted to floats by pandas. Yuck!
+    _new_chromosome = "NA"
+    _new_position = "NA"
+    try:
+        p = int(position)
+        l_ = l.convert_coordinate(chromosome, p)
+        if l_ is None:
+            raise ValueError("Chromosome {} and position {}".format(chromosome, position))
+        if l_:
+            if len(l_) > 1:
+                logging.warning("Liftover with more than one candidate: %s", t.variant_id)
+            _new_chromosome = l_[0][0]
+            _new_position = int(l_[0][1])
+    except:
+        pass
+    return _new_chromosome, _new_position
+
+def liftover(args, d):
+    logging.info("Performing liftover")
+    l = pyliftover.LiftOver(args.liftover)
+    new_position = []
+    new_chromosome = []
+    for t in d.itertuples():
+        _new_chromosome, _new_position = _lift(l, t.chromosome, t.position)
+
+        new_chromosome.append(_new_chromosome)
+        new_position.append(_new_position)
+
+    d = d.assign(chromosome=new_chromosome)
+    d = d.assign(position=new_position)
+    d = d[d.chromosome.astype(str) !="NA"]
+    d = d[d.position.astype(str) != "NA"]
+
+    logging.log(9, "%d variants after liftover", d.shape[0])
+    return d
+
+def lift_metadata(metadata, lift_fp):
+    l = pyliftover.LiftOver(lift_fp)
+    new_position = []
+    new_chromosome = []
+    for t in metadata.itertuples():
+        _new_chrom, _new_pos = _lift(l, t.chromosome, t.position)
+        new_chromosome.append(_new_chrom)
+        new_position.append(_new_pos)
+    metadata = metadata.assign(chromosome = new_chromosome)
+    metadata = metadata.assign(position = new_position)
+
+    metadata = metadata[metadata.chromosome.astype(str) != "NA"]
+    metadata = metadata[metadata.position.astype(str) != "NA"]
+    logging.log(9, "%d variants after liftover", metadata.shape[0])
+    metadata = metadata.rename({'variant_id': 'geno_id'}, axis=1)
+    metadata = canonical_variant_id(metadata, name='model_id')
+
+    return metadata['geno_id', 'model_id']
+
+
+
+def canonical_variant_id(d, name='id', allele_0='allele_0', allele_1='allele_1'):
+    cols = ['chromosome', 'position', allele_1, allele_0]
+    for i in cols:
+        if i not in d:
+            raise ValueError("To create variant ID need column: {}".format(i))
+
+    d[name] = (d['chromosome'].astype(str) + '_'
+                       + d['position'].astype(str) + "_"
+                       + d[allele_0] + "_" + d[allele_1])
+    return d
+
+def load_gene_d(dosage, model_variants, geno_variants=None, individuals=None):
+    if geno_variants is None:
+        geno_variants = model_variants
+        g_bool = False
+    else:
+        g_bool = True
+    if individuals:
+        dd = Parquet._read(dosage, columns=geno_variants, specific_individuals=individuals)
+        del dd["individual"]
+    else:
+        dd = Parquet._read(dosage, columns=geno_variants, skip_individuals=True)
+    if g_bool:
+        for i in range(len(geno_variants)):
+            dd[model_variants[i]] = dd.pop(geno_variants[i])
+
+    return dd
+
+
+
 n_ = re.compile("^(\d+)$")
 
 def run(args):
     if os.path.exists(args.output):
         logging.info("Output already exists, either delete it or move it")
         return
+
+    if args.parquet_metadata is not None and args.metadata_lift is not None:
+        logging.info("Loading metadata for liftover")
+        metadata = pq.read_table(args.parquet_metadata).to_pandas()
+        logging.log(9, "%d variants before liftover", metadata.shape[0])
+        lifted_variants = lift_metadata(metadata, args.metadata_lift)
+    else:
+        lifted_variants = None
 
     logging.info("Getting parquet genotypes")
     file_map = get_file_map(args)
@@ -54,30 +151,31 @@ def run(args):
                 gene_d = {}
                 logging.log(9, "Proccessing %i/%i:%s", i+1, extra.shape[0], g_)
                 w = pandas.read_sql("select * from weights where gene = '{}';".format(g_), connection)
+                if lifted_variants is not None:
+                    w = w.merge(lifted_variants, left_on='varID', right_on='model_id')
                 w['chr'] = [x[0] for x in w['varID'].str.split("_")]
                 w['chr'] = w['chr'].str.lstrip('chr')
-                chr_lst = list(w['chr'].unique())
-                if len(chr_lst) == 0:
+                w = w.groupby('chr')
+                if len(w.groups.keys()) == 0:
                     logging.log(9, "No chromosomes found for %s, skipping", g_)
                     continue
-                for chr_ in chr_lst:
+                for chr_, w_chr in w:
                     if not n_.search(chr_):
                         logging.log(9, "Unsupported chromosome: %s", chr_)
                         continue
                     dosage = file_map[int(chr_)]
-
-                    if individuals:
-                        gene_d.update(Parquet._read(dosage, columns=w.varID.values, specific_individuals=individuals))
-                        del gene_d["individual"]
+                    if 'geno_id' in w:
+                        gene_d.update(load_gene_d(dosage,
+                                                  w_chr.varID.values,
+                                                  geno_variants=w_chr.geno_id.values,
+                                                  individuals=individuals))
                     else:
-                        gene_d.update(Parquet._read(dosage, columns=w.varID.values, skip_individuals=True))
+                        gene_d.update(load_gene_d(dosage,
+                                                  w_chr.varID.values,
+                                                  individuals=individuals))
 
                 var_ids = list(gene_d.keys())
                 if len(var_ids) == 0:
-               #     if len(w.varID.values) == 1:
-               #         logging.log(9, "workaround for single missing genotype at %s", g_)
-               #         gene_d = {w.varID.values[0]:[0,1]}
-               #     else:
                     logging.log(9, "No genotype available for %s, skipping",g_)
                     logging.log(4, 'Could not find {}'.format(w.varID.values[:5]))
                     next
@@ -103,6 +201,8 @@ if __name__ == "__main__":
     parser.add_argument("-model_db", help="Where to save stuff")
     parser.add_argument("-output", help="Where to save stuff")
     parser.add_argument("--output_rsids", action="store_true")
+    parser.add_argument("--metadata_lift")
+    parser.add_argument("--parquet_metadata")
     parser.add_argument("--individuals")
     parser.add_argument("-parsimony", help="Log verbosity level. 1 is everything being logged. 10 is only high level messages, above 10 will hardly log anything", default = "10")
 
